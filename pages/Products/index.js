147038c6215da7e02ec23ae05c5fe206a4932513
pedro-pauletti <paulettipedrohenrique@gmail.com>
2022-11-26 02:51:49 -0300

    Products page updated

--- pages/Products/index.js
+++ pages/Products/index.js
@@ -4,56 +4,24 @@ import Product from '../../components/Product';
 import { FetchProductsByID } from '../../config/Config';
 
 
-const StaticMarketImage = "https://static.paodeacucar.com/img/uploads/1/35/12467035.png"
-
-
-const test = [
-  { name: "Produto 1", brand: "Marca 1", barCode: "55555555555", image: StaticMarketImage },
-  { name: "Produto 2", brand: "Marca 2", barCode: "55555555555", image: StaticMarketImage },
-  { name: "Produto 3", brand: "Marca 3", barCode: "55555555555", image: StaticMarketImage },
-  { name: "Produto 4", brand: "Marca 4", barCode: "55555555555", image: StaticMarketImage },
-  { name: "Produto 5", brand: "Marca 5", barCode: "55555555555", image: StaticMarketImage },
-  { name: "Produto 6", brand: "Marca 6", barCode: "55555555555", image: StaticMarketImage },
-  { name: "Produto 7", brand: "Marca 7", barCode: "55555555555", image: StaticMarketImage },
-];
-
-console.log(test)
-
-
 function Products({ route }) {
 
   const marketName = route.params.name
+  const marketImage = route.params.image
   const products = route.params.products
 
-  // const [products, setProducts] = useState([])
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const produtos = await FetchAllProductsById(productsList)
-  //     setProducts(produtos)
-  //   }
-
-  //   fetchData()
-  //     .catch(console.error);
-  // }, [])
 
 
-  // async function FetchAllProductsById(products) {
-  //   let productsList = []
-  //   products.forEach(product => {
-  //     const productJson = await FetchProductsByID(product.idProduto.id)
-  //     productsList.push(productJson)
-  //   });
-  //   return productsList
-  // }
+  console.log(marketName)
+  console.log(marketImage)
 
-
-  console.log("produtos extraidos")
-  console.log(products)
 
   return (
     <View style={styles.container}>
-      <Text>Lista de Produtos do Mercado {marketName}</Text>
+      <View style={{width: '100%', marginHorizontal: '50%', flexDirection:'row', backgroundColor:'white', paddingTop:40}}>
+        <Image style= {{width:50, height: 50, alignItems:'left'}} source={{uri: marketImage}}/>
+        <Text style={styles.textTitle}>Lista de Produtos do Mercado {marketName}</Text>
+      </View>
       <SafeAreaView>
         <FlatList data={products}
           keyExtractor={(item, index) => index.toString()}
@@ -72,5 +40,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textTitle: {
+    textAlign: 'center',
+    marginTop:20,
+    marginLeft:25,
+    fontSize: 14,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    color: 'black',
   },
 });
